b76033e7af4203ba2ccfd49753db7f179f529b94
Julien Henry <julien.henry@posteo.net>
2025-01-20 17:40:25 +0100

    modified regex for comparison

--- v3.6.3/script.R
+++ v3.6.3/script.R
@@ -1,13 +1,30 @@
 
 
-begin_str <- paste0("{", paste0(rep('a', 99), collapse = ""), collapse = "")
-end_str <- paste0(paste0(rep('a', 99), collapse = ""), "}", collapse = "")
-final_str <- paste0(paste0(rep(begin_str, times = 1000), collapse = ""), paste0(rep(end_str, times = 1000), collapse = ""), collapse = "")
-
 library(microbenchmark)
-res <- microbenchmark(regexpr(pattern = '\\{(?>[^{}]*|(?R))*\\}', text = final_str, perl = T), 
-                      times = 1000)
+text_file <- 'mtent12.txt'
+file_str <- readChar(text_file, file.info(text_file)$size)
+file_str <- iconv(file_str, "UTF-8", "UTF-8", sub = "")
 
-saveRDS(res, "/output/results.rds")
+patterns <- c(
+  "Twain",
+  "Huck[[:alpha:]]+",
+  "[[:alpha:]]+ing",
+  "^[^ ]*?Twain",
+  "Tom|Sawyer|Huckleberry|Finn",
+  " (Tom|Sawyer|Huckleberry|Finn).{0,30}river|river.{0,30}(Tom|Sawyer|Huckleberry|Finn)"
+)
+
+lres <- list()
+for (p in patterns) {
+  
+  lres[[p]] <- microbenchmark(
+    gregexpr(pattern = p, text = file_str, perl = T),
+    times = 100, 
+    unit = "milliseconds"
+  )
+  
+}
+
+saveRDS(lres, "/output/results.rds")
 
 
